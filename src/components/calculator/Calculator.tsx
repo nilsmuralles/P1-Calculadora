a9552a86c0c33c5304ca87f2220ff5f7902254a2
nilsmuralles <murallesnils11@gmail.com>
2025-05-27 11:05:47 -0600

    refactor: smaller components

--- src/components/calculator/Calculator.tsx
+++ src/components/calculator/Calculator.tsx
@@ -2,41 +2,18 @@ import Button from '../button/Button'
 import Display from '../display/Display'
 import useCalculator from '../../hooks/useCalculator'
 import './Calculator.css'
-
 const Calculator = () => {
   const { display, inputNumber, inputOperation, calculate, clear } = useCalculator()
-
+  const buttons = ['C', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '', '.', '=']
+  const handlers: Record<string, () => void> = { C: clear, '+/-': () => inputOperation('+/-'), '%': () => inputOperation('%'), '÷': () => inputOperation('/'), 'x': () => inputOperation('x'), '-': () => inputOperation('-'), '+': () => inputOperation('+'), '=': calculate, '.': () => inputNumber('.'), '': () => {}, ...Object.fromEntries(['0','1','2','3','4','5','6','7','8','9'].map(n => [n, () => inputNumber(n)]))
+  }
   return (
-    <>
-      <main>
-        <section className='display-container'>
-          <Display value={ display }/>
-        </section>
-        <section className='buttons-container'>
-          <Button label='C' onClick={ clear }/>
-          <Button label='+/-' onClick={()=>{ inputOperation('+/-') }}/>
-          <Button label='%' onClick={()=>{ inputOperation('%') }}/>
-          <Button label='÷' onClick={()=>{ inputOperation('/') }}/>
-          <Button label='7' onClick={()=>{ inputNumber('7') }}/>
-          <Button label='8' onClick={()=>{ inputNumber('8') }}/>
-          <Button label='9' onClick={()=>{ inputNumber('9') }}/>
-          <Button label='x' onClick={()=>{ inputOperation('x') }}/>
-          <Button label='4' onClick={()=>{ inputNumber('4') }}/>
-          <Button label='5' onClick={()=>{ inputNumber('5') }}/>
-          <Button label='6' onClick={()=>{ inputNumber('6') }}/>
-          <Button label='-' onClick={()=>{ inputOperation('-') }}/>
-          <Button label='1' onClick={()=>{ inputNumber('1') }}/>
-          <Button label='2' onClick={()=>{ inputNumber('2') }}/>
-          <Button label='3' onClick={()=>{ inputNumber('3') }}/>
-          <Button label='+' onClick={()=>{ inputOperation('+') }}/>
-          <Button label='0' onClick={()=>{ inputNumber('0') }}/>
-          <Button label=' ' onClick={()=>{}}/>
-          <Button label='.' onClick={()=>{ inputNumber('.') }}/>
-          <Button label='=' onClick={ calculate }/>
-        </section>
-      </main>
-    </>
+    <main>
+      <section className='display-container'><Display value={display} /></section>
+      <section className='buttons-container'>
+        {buttons.map(b => <Button key={b} label={b} onClick={handlers[b]} />)}
+      </section>
+    </main>
   )
 }
-
 export default Calculator
